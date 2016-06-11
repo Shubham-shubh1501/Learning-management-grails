a80800d65fc1a6e3c6313f7ea4d98c67e90414e4
Shubham Jain S <shubham.shubh1501@gmail.com>
2016-06-11 13:50:48 +0530

    Added user.groovy

---
+++ src/groovy/learning/management/User.groovy
@@ -0,0 +1,17 @@
+package learning.management
+
+/**
+ * Created by shubham on 11/6/16.
+ */
+class User {
+    String fname
+    String lname
+    String eml
+    String age
+    void myTask(){
+        fname = fname.toUpperCase()
+        lname = lname.toUpperCase()
+        eml  = eml.toUpperCase()
+    }
+
+}
