d9faf5e40927b9eb824553da88a9bb2cbd3fcc34
Shubham Jain S <shubham.shubh1501@gmail.com>
2016-06-11 12:23:45 +0530

    modified hello and Jsp

--- grails-app/controllers/learning/management/HelloController.groovy
+++ grails-app/controllers/learning/management/HelloController.groovy
@@ -7,5 +7,10 @@ class HelloController {
     }
 
     def signup() {
+        Map m = [firstName: "shubham", lastName: "jain S",Email:"Shubham-shubh1501@gmail.com", password:"Xyz"]
+        return m
+    }
+    def register(){
+        render(view: "signup")//use of render
     }
 }
